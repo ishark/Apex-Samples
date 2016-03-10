6fbf39351ff1e7743b877494c9085583abd2a236
ishark <isha@datatorrent.com>
2016-03-09 16:45:09 -0800

    set partition count and max count

--- sample-hdht-app/src/main/java/com/example/hdht/sample/Application.java
+++ sample-hdht-app/src/main/java/com/example/hdht/sample/Application.java
@@ -8,15 +8,11 @@ import org.apache.hadoop.conf.Configuration;
 import com.datatorrent.api.annotation.ApplicationAnnotation;
 import com.datatorrent.api.StreamingApplication;
 import com.datatorrent.api.DAG;
-import com.datatorrent.api.DAG.Locality;
 import com.datatorrent.lib.fileaccess.TFileImpl;
-import com.datatorrent.lib.io.ConsoleOutputOperator;
-import com.datatorrent.lib.io.fs.AbstractFileInputOperator.FileLineInputOperator;
 
 @ApplicationAnnotation(name="MyFirstApplication")
 public class Application implements StreamingApplication
 {
-
   @Override
   public void populateDAG(DAG dag, Configuration conf)
   {
@@ -26,15 +22,14 @@ public class Application implements StreamingApplication
     RandomKeyValGenerator randomGenerator = dag.addOperator("randomGenerator", new RandomKeyValGenerator());
     randomGenerator.setNumTuples(100000);
     HdhtStoreOperator store = dag.addOperator("hdht", new HdhtStoreOperator());
-    store.setPartitionCount(4);
+    store.setPartitionCount(3);
+    store.setMaxPartitionCount(12);
     String basePath = "/user/isha/hdht";
     TFileImpl hdsFile = new TFileImpl.DTFileImpl();
     basePath += "/" + System.currentTimeMillis();
     hdsFile.setBasePath(basePath);
     System.out.println("Setting basePath " + basePath);
     store.setFileStore(hdsFile);
-    
-//    ConsoleOutputOperator cons = dag.addOperator("console", new ConsoleOutputOperator());
 
     dag.addStream("randomData", randomGenerator.out, store.input);//.setLocality(Locality.CONTAINER_LOCAL);
   }
