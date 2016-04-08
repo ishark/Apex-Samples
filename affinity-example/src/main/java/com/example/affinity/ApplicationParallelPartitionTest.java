aa53d7d28e27ee26a60b1745a6bf822725ea9cc2
ishark <isha@datatorrent.com>
2016-04-08 16:46:42 -0700

    Update APIs

--- affinity-example/src/main/java/com/example/affinity/ApplicationParallelPartitionTest.java
+++ affinity-example/src/main/java/com/example/affinity/ApplicationParallelPartitionTest.java
@@ -3,10 +3,17 @@
  */
 package com.example.affinity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.hadoop.conf.Configuration;
 
+import com.datatorrent.api.AffinityRule.Type;
+import com.datatorrent.api.Context.DAGContext;
 import com.datatorrent.api.Context.OperatorContext;
 import com.datatorrent.api.Context.PortContext;
+import com.datatorrent.api.AffinityRule;
+import com.datatorrent.api.AffinityRulesSet;
 import com.datatorrent.api.DAG;
 import com.datatorrent.api.DAG.Locality;
 import com.datatorrent.api.StreamingApplication;
@@ -15,7 +22,7 @@ import com.datatorrent.common.partitioner.StatelessPartitioner;
 import com.datatorrent.lib.algo.UniqueCounter;
 import com.datatorrent.lib.io.ConsoleOutputOperator;
 
-@ApplicationAnnotation(name="ApplicationParallelPartitionTest")
+@ApplicationAnnotation(name = "ApplicationParallelPartitionTest")
 public class ApplicationParallelPartitionTest implements StreamingApplication
 {
 
@@ -28,13 +35,18 @@ public class ApplicationParallelPartitionTest implements StreamingApplication
     ConsoleOutputOperator console = dag.addOperator("console", new ConsoleOutputOperator());
     dag.addStream("rand_calc", rand.out, counter.data).setLocality(Locality.CONTAINER_LOCAL);
     dag.addStream("passthru1", counter.count, passThru1.input);
-    dag.addStream("rand_console",passThru1.output, console.input);
+    dag.addStream("rand_console", passThru1.output, console.input);
     dag.setAttribute(rand, OperatorContext.PARTITIONER, new StatelessPartitioner<RandomNumberGenerator>(5));
 
-    dag.setAntiAffinity(Locality.NODE_LOCAL, false, "rand", "rand");
-    dag.setInputPortAttribute(counter.data,PortContext.PARTITION_PARALLEL, true);
-    dag.setInputPortAttribute(passThru1.input,PortContext.PARTITION_PARALLEL, true);
-    dag.setInputPortAttribute(console.input,PortContext.PARTITION_PARALLEL, true);
-    
+    AffinityRulesSet ruleSet = new AffinityRulesSet();
+    List<AffinityRule> rules = new ArrayList<>();
+    rules.add(new AffinityRule(Type.ANTI_AFFINITY, Locality.NODE_LOCAL, false, "rand", "rand"));
+    ruleSet.setAffinityRules(rules);
+
+    dag.setAttribute(DAGContext.AFFINITY_RULES_SET, ruleSet);
+    dag.setInputPortAttribute(counter.data, PortContext.PARTITION_PARALLEL, true);
+    dag.setInputPortAttribute(passThru1.input, PortContext.PARTITION_PARALLEL, true);
+    dag.setInputPortAttribute(console.input, PortContext.PARTITION_PARALLEL, true);
+
   }
 }
--- affinity-example/src/main/java/com/example/affinity/ApplicationTestStreamLocal.java
+++ affinity-example/src/main/java/com/example/affinity/ApplicationTestStreamLocal.java
@@ -1,9 +1,16 @@
 package com.example.affinity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.hadoop.conf.Configuration;
 
+import com.datatorrent.api.AffinityRule;
+import com.datatorrent.api.AffinityRulesSet;
 import com.datatorrent.api.DAG;
 import com.datatorrent.api.StreamingApplication;
+import com.datatorrent.api.AffinityRule.Type;
+import com.datatorrent.api.Context.DAGContext;
 import com.datatorrent.api.Context.OperatorContext;
 import com.datatorrent.api.DAG.Locality;
 import com.datatorrent.api.annotation.ApplicationAnnotation;
@@ -11,7 +18,7 @@ import com.datatorrent.common.partitioner.StatelessPartitioner;
 import com.datatorrent.lib.algo.UniqueCounter;
 import com.datatorrent.lib.io.ConsoleOutputOperator;
 
-@ApplicationAnnotation(name="AffinityRulesContainerLocal")
+@ApplicationAnnotation(name = "AffinityRulesContainerLocal")
 public class ApplicationTestStreamLocal implements StreamingApplication
 {
 
@@ -23,17 +30,21 @@ public class ApplicationTestStreamLocal implements StreamingApplication
     PassThruOperator passThru1 = dag.addOperator("passThru", new PassThruOperator());
     ConsoleOutputOperator console = dag.addOperator("console", new ConsoleOutputOperator());
     ConsoleOutputOperator console1 = dag.addOperator("console1", new ConsoleOutputOperator());
-    
-    dag.addStream("rand_calc", rand.out, counter.data,passThru1.input).setLocality(Locality.NODE_LOCAL);
-    
-    dag.addStream("rand_console",passThru1.output, console.input);
-    dag.addStream("counter_console",counter.count, console1.input);
+
+    dag.addStream("rand_calc", rand.out, counter.data, passThru1.input).setLocality(Locality.NODE_LOCAL);
+
+    dag.addStream("rand_console", passThru1.output, console.input);
+    dag.addStream("counter_console", counter.count, console1.input);
 
     dag.setAttribute(passThru1, OperatorContext.PARTITIONER, new StatelessPartitioner<PassThruOperator>(5));
-    
-    dag.setAffinity(Locality.CONTAINER_LOCAL, false, "rand", "console", "counter");
-//    dag.setAntiAffinity(Locality.NODE_LOCAL, false, "console", "passThru");
-   
-    dag.getMeta(rand).getAttributes().put(OperatorContext.LOCALITY_HOST, "node17.morado.com");    
+
+    AffinityRulesSet ruleSet = new AffinityRulesSet();
+    List<AffinityRule> rules = new ArrayList<>();
+    rules.add(new AffinityRule(Type.AFFINITY, Locality.CONTAINER_LOCAL, false, "rand", "console", "counter"));
+    ruleSet.setAffinityRules(rules);
+
+    dag.setAttribute(DAGContext.AFFINITY_RULES_SET, ruleSet);
+
+    dag.getMeta(rand).getAttributes().put(OperatorContext.LOCALITY_HOST, "node17.morado.com");
   }
 }
